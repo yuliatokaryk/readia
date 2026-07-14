b41f915e78b04587ee8c00eb956b1ea0e70a869c
yuliatokaryk <julia4099@gmail.com>
2026-07-14 13:44:44 -0600

    PGNL-6 - Generated Books, added minimal styles, set root path to books#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "books#index"
 end
