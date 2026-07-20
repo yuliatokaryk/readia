d9294b052599f656fdea5e63ad0d4b4bfb717bc5
yuliatokaryk <julia4099@gmail.com>
2026-07-20 15:53:58 -0600

    PGNL-9 - Added policy to policies classes and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 end
--- app/policies/book_policy.rb
+++ app/policies/book_policy.rb
@@ -1,2 +1,16 @@
-class BookPolicy
+class BookPolicy < ApplicationPolicy
+  attr_reader :user, :book
+
+  def initialize(user, book)
+    @user = user
+    @book = book
+  end
+
+  def update?
+    book.user == user
+  end
+
+  def destroy?
+    book.user == user
+  end
 end
