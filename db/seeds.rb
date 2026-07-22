2142fe72416dc8d4c52aefbb81542129826722ec
yuliatokaryk <julia4099@gmail.com>
2026-07-21 21:10:05 -0600

    PGNL-12 - Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "ffaker"
+
+USERS_COUNT = 5
+
+users = (1..USERS_COUNT).each.map do |i|
+  email = "user_#{i}@gmail.com"
+  User.create!(
+    email: email,
+    password: "Password123!"
+  )
+end
+
+authors = 10.times.map do
+  Author.create!(
+    user: users.sample,
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+  )
+end
+
+10.times do
+  Book.create!(
+    user: users.sample,
+    title: FFaker::Book.title,
+    author: authors.sample
+  )
+end
