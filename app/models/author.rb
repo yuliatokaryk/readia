abe9cd1306c276d8c59919b5948e935b3b31b2f6
yuliatokaryk <julia4099@gmail.com>
2026-07-24 19:10:41 -0600

    PGNL-20 - Added active storage

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,8 @@
 class Author < ApplicationRecord
   belongs_to :user
   has_many :books, dependent: :destroy
+  has_one_attached :portrait
+
   validates :first_name, presence: true
 
   def full_name
